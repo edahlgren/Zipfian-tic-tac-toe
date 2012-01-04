82160ae70956a7d12ad945b1f2aa142b4640f007
Erin Dahlgren <edahlgren@uchicago.edu>
2012-01-03 20:08:18 -0600

    A main module for running the whole game, with an intro prompt

---
+++ play.py
@@ -0,0 +1,22 @@
+from Engine import *
+from Interact import *
+from Presentation import *
+
+"""
+A main module for running The George Kingsley Zipf AI Engine and the game logic
+
+Author: Erin Dahlgren
+
+Date: November 2011
+
+"""
+
+def main():
+        make_space(15)
+        print "Welcome to a glorious day of Tic Tac Toe."
+        print "Your opponent is George Kingsley Zipf."
+        choose_action()
+
+
+if __name__ == "__main__":
+	main()
